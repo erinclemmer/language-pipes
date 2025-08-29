92d705a99f48a1d8194eeb7801b4dfcf98f55ae9
erinclemmer <erin.c.clemmer@gmail.com>
2025-08-28 19:27:58 -0500

    create get_version fn

--- src/language_pipes/cli.py
+++ src/language_pipes/cli.py
@@ -1,15 +1,18 @@
 import os
 import sys
+import argparse
 from language_pipes.util.aes import generate_aes_key
 
 from __init__ import LanguagePipes
 
-def main():
-
+def get_version():
     if not os.path.exists("release.txt"):
         raise Exception("Could not find release.txt file")
     with open("release.txt", "r", encoding="utf-8") as f:
         version = f.read()
+
+def main():
+    version = get_version()
 
     if len(sys.argv) < 2:
         print("Usage: language_pipes [config_file]")
