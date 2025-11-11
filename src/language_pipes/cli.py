c98e54f8c98a219d074b71c0385acd9cdfb05a25
erinclemmer <erin.c.clemmer@gmail.com>
2025-11-10 20:23:33 -0600

    Update version

--- src/language_pipes/cli.py
+++ src/language_pipes/cli.py
@@ -6,7 +6,7 @@ from language_pipes.util.aes import generate_aes_key
 
 from language_pipes import LanguagePipes
 
-VERSION = "0.5.2"
+VERSION = "0.6.0"
 
 def build_parser():
     parser = argparse.ArgumentParser(description="Language Pipes CLI")
