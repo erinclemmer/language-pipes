d78b0da9c381c5587fb7741fcde6fe36a44803ea
erinclemmer <erin.c.clemmer@gmail.com>
2026-01-01 11:59:11 -0600

    Version bump

--- src/language_pipes/cli.py
+++ src/language_pipes/cli.py
@@ -12,7 +12,7 @@ from language_pipes.commands.upgrade import upgrade_lp
 
 from language_pipes import LanguagePipes
 
-VERSION = "0.14.0"
+VERSION = "0.15.0"
 
 def build_parser():
     parser = argparse.ArgumentParser(
