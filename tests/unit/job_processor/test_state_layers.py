3b20ffb1346521bf9af2860442d35dc94bcb6943
erinclemmer <erin.c.clemmer@gmail.com>
2026-01-11 17:13:26 -0600

    split apart job processing tests

---
+++ tests/unit/job_processor/test_state_layers.py
@@ -0,0 +1,75 @@
+import os
+import sys
+import torch
+import unittest
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..', '..', 'src'))
+
+from language_pipes.config import LpConfig
+from language_pipes.jobs.job import Job
+from language_pipes.jobs.job_data import JobData
+from language_pipes.jobs.job_processor import JobContext, JobProcessor, JobState
+from language_pipes.network.config import NetworkConfig
+from language_pipes.util.enums import ComputeStep, JobStatus
+
+from tests.unit.job_processor.util import make_processor, make_job, FakeEndModel, FakePipeMulti, FakeModel, TrackingModel, EmptyPipe
+
+class TestProcessLayersState(unittest.TestCase):
+    """Tests for the _state_process_layers method."""
+
+    def test_transitions_to_done_when_local_model_missing(self):
+        job = make_job()
+        job.compute_step = ComputeStep.LAYER
+        job.current_layer = 0
+        job.data = JobData()
+        job.data.state = torch.zeros((1, 1))
+
+        processor = make_processor(
+            job=job,
+            pipe=EmptyPipe(FakeModel("node-a", 0, 0)),
+            end_model=None,
+        )
+
+        next_state = processor._state_process_layers()
+
+        self.assertEqual(next_state, JobState.DONE)
+
+    def test_transitions_to_send_for_remote_layer(self):
+        job = make_job()
+        job.compute_step = ComputeStep.LAYER
+        job.current_layer = 0
+        job.data = JobData()
+        job.data.state = torch.zeros((1, 1))
+        job.last_update = 0
+
+        local_model = TrackingModel("node-a", 0, 0, virtual=False, num_hidden_layers=2)
+        remote_model = FakeModel("node-b", 1, 1, virtual=True)
+        pipe = FakePipeMulti(local_model, remote_model)
+
+        processor = make_processor(job=job, pipe=pipe, end_model=None)
+
+        next_state = processor._state_process_layers()
+
+        self.assertEqual(next_state, JobState.SEND)
+        self.assertTrue(local_model.processed)
+        self.assertGreater(job.last_update, 0)
+
+    def test_transitions_to_process_layers_for_local_segment(self):
+        job = make_job()
+        job.compute_step = ComputeStep.LAYER
+        job.current_layer = 0
+        job.data = JobData()
+        job.data.state = torch.zeros((1, 1))
+        job.last_update = 0
+
+        local_model = TrackingModel("node-a", 0, 0, virtual=False, num_hidden_layers=2)
+        next_model = FakeModel("node-a", 1, 1, virtual=False)
+        pipe = FakePipeMulti(local_model, next_model)
+
+        processor = make_processor(job=job, pipe=pipe, end_model=None)
+
+        next_state = processor._state_process_layers()
+
+        self.assertEqual(next_state, JobState.PROCESS_LAYERS)
+        self.assertTrue(local_model.processed)
+        self.assertGreater(job.last_update, 0)
