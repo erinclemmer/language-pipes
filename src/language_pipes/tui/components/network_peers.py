01317a8b2e7645544861bcd19e219cb73c2a4a08
erinclemmer <erin.c.clemmer@gmail.com>
2026-04-03 14:21:51 -0500

    Display 'No peers connected' message in Network Peers page when empty

--- src/language_pipes/tui/components/network_peers.py
+++ src/language_pipes/tui/components/network_peers.py
@@ -5,17 +5,20 @@ from language_pipes.tui.content_loader import ContentLoader
 from language_pipes.tui.frame.provider_calls import ProviderCall
 from language_pipes.distributed_state_network.objects.state_packet import StatePacket
 
+
 class NetworkPeers:
     loader: ContentLoader
     exit_page: Callable
     is_focused: Callable
     peers: Dict[str, StatePacket]
 
-    def __init__(self, loader: ContentLoader, exit_page: Callable, is_focused: Callable):
+    def __init__(
+        self, loader: ContentLoader, exit_page: Callable, is_focused: Callable
+    ):
         self.loader = loader
         self.exit_page = exit_page
         self.is_focused = is_focused
-        self.peers = { }
+        self.peers = {}
 
     def on_key(self, key: PressedKey, ch: str):
         if key == PressedKey.Escape:
@@ -23,11 +26,13 @@ class NetworkPeers:
 
     def get_view(self) -> List[str]:
         self.peers = self.loader.call_provider(ProviderCall.list_peers)
+        if not self.peers:
+            return ["Network Peers:", "", "No peers connected"]
         lines = ["Network Peers:", ""]
         for key in self.peers.keys():
             lines.append(f"- {key}")
 
         return lines
-    
+
     def get_footer(self) -> str:
         return ""
