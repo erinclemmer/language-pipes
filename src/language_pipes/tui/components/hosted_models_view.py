bb7d9dd11da28f33fd81dc97bd3e5631bd092cc9
erinclemmer <erin.c.clemmer@gmail.com>
2026-05-17 19:48:29 -0500

    normalize pipe strings

--- src/language_pipes/tui/components/hosted_models_view.py
+++ src/language_pipes/tui/components/hosted_models_view.py
@@ -25,9 +25,33 @@ def format_pipe_strings(running: List[ModelStatusInfo]) -> List[str]:
                 pipe_string[i] = ch
         pipe_strings[key] = ''.join(pipe_string)
 
+    norm_pipe_strings = { }
+    for key in pipes.keys():
+        pipe = pipes[key]
+        pipe_string = ["X" for _ in range(0, 28)]
+        layers_per_char =  pipe[0].num_layers / 28.0
+        for i in range(0, 28):
+            start = int(layers_per_char * i)
+            end = int(layers_per_char * (i + 1))
+            loading = False
+            loaded = True
+            for c in pipe_strings[key][start:end]:
+                if c == "|":
+                    loading = True
+                    break
+                if c == "X":
+                    loaded = False
+                    break
+            
+            if loading:
+                pipe_string[i] = "|"
+            elif loaded:
+                pipe_string[i] = "="
+        norm_pipe_strings[key] = "".join(pipe_string)
+    
     lines = []
     for key in pipe_strings.keys():
-        lines.append(f"Pipe {key[:4]} >{pipe_strings[key]}<")
+        lines.append(f"Pipe {key[:4]} >{norm_pipe_strings[key]}<")
 
     return lines
 
